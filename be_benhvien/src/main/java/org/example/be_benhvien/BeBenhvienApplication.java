2bf3dd09df75638fe628c5133314d08ba2bedf22
bpbn <bpbngoc11@gmail.com>
2024-07-14 14:04:14 +0700

    tạo project

---
+++ be_benhvien/src/main/java/org/example/be_benhvien/BeBenhvienApplication.java
@@ -0,0 +1,13 @@
+package org.example.be_benhvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBenhvienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBenhvienApplication.class, args);
+	}
+
+}
