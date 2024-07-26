48c4a8f1b6b0eb4d8c55d0fb76762362d65acc37
bpbn <bpbngoc11@gmail.com>
2024-07-26 13:29:08 +0700

    add themPhieuHen

---
+++ be_benhvien/src/main/java/org/example/be_benhvien/controller/PhieuHenController.java
@@ -0,0 +1,26 @@
+package org.example.be_benhvien.controller;
+
+import org.example.be_benhvien.DAO.PhieuHenDAO;
+import org.example.be_benhvien.POJO.PhieuHenPOJO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/phieuhen")
+public class PhieuHenController {
+
+    private final PhieuHenDAO phieuHenDAO;
+
+    @Autowired
+    public PhieuHenController(PhieuHenDAO phieuHenDAO) {
+        this.phieuHenDAO = phieuHenDAO;
+    }
+
+    @PostMapping("/themPH")
+    public void themPhieuHen(@RequestBody PhieuHenPOJO phieuHenPOJO) {
+        phieuHenDAO.themPhieuHen(phieuHenPOJO);
+    }
+
+
+}
